b8af750ee112c1ef2837fbd2deac9e5142d4d8e0
nikkimay09 <nikkimay09@gmail.com>
2021-10-17 23:33:39 -0400

    part 2

--- Parstagram/CommentCell.swift
+++ Parstagram/CommentCell.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class CommentCell: UITableViewCell {
-
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Podfile
+++ Podfile
@@ -9,6 +9,7 @@ target 'Parstagram' do
 	# Add Parse pods and install
  	pod 'Parse'
  	pod 'AlamofireImage'
+	pod 'MessageInputBar'
 
   target 'ParstagramTests' do
     inherit! :search_paths
