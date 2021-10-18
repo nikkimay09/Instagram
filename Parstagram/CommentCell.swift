6ade9af0961afe2e11b8e03ddd06beb450ffe00a
nikkimay09 <nikkimay09@gmail.com>
2021-10-17 23:31:55 -0400

    part 2

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Nikki Bandaru on 10/16/21.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
